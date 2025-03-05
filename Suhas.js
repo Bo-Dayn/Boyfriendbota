3ccafdbff513f0ba743d565669853c7f0493dcbf
Science Officer <120959377+Bo-Dayn@users.noreply.github.com>
2025-03-05 19:31:02 +0530

    Update Suhas.js

--- Suhas.js
+++ Suhas.js
@@ -1,3 +1,3 @@
 {
-  "zipmegalink": "https://mega.nz/file/bCokgALb#nFzPbFzEzglzj7ouPzHs6f6fmn2fR-zzmhd4Ht8Ah38"
+  "zipmegalink": "https://mega.nz/file/2HAHjIiS#K0_eUe2HJmSamMvYn4c9pDZMGJ7ZwnZcZwxHDnQqQvk"
 }
